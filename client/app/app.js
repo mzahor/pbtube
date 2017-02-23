a34d6bbea7f3c880e186a2cb60fb71a589009dce
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2017-02-23 17:25:29 +0200

    fix: app module registration

--- client/app/app.js
+++ client/app/app.js
@@ -1,18 +1,18 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import videoFeed from './videoFeed/videoFeed';
-import AppComponent from './app.component';
+import appComponent from './app.component';
 import 'normalize.css';
 
-angular.module('app', [
-    uiRouter,
-    videoFeed
-  ])
-  .config(($locationProvider) => {
-    'ngInject';
-    // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
-    // #how-to-configure-your-server-to-work-with-html5mode
-    $locationProvider.html5Mode(true).hashPrefix('!');
-  })
+const appModule = angular.module('app', [
+  uiRouter,
+  videoFeed
+])
+.config(($locationProvider) => {
+  'ngInject';
+  // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
+  // #how-to-configure-your-server-to-work-with-html5mode
+  $locationProvider.html5Mode(true).hashPrefix('!');
+});
 
-  .component('app', AppComponent);
+appComponent.register(appModule);
