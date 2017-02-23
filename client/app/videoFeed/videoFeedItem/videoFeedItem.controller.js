4b62acb4ac637bc46a2829029d6b7dc59aa59225
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2017-02-23 15:09:27 +0200

    feat: render fb videos

--- client/app/videoFeed/videoFeedItem/videoFeedItem.controller.js
+++ client/app/videoFeed/videoFeedItem/videoFeedItem.controller.js
@@ -15,8 +15,16 @@ class VideoFeedItemController {
     return this.video.views
   }
 
+  getVideoSource() {
+    return this.video.source;
+  }
+
   getVideoId() {
     return this.video.videoId;
+  }
+
+  getVideoUrl() {
+    return 'https://www.facebook.com/facebook/videos/' + this.video.videoId;
   }
 }
 
