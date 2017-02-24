20a2bf6ff89b35c690afe77eef5f6bf3b71f0c96
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2017-02-24 11:20:43 +0200

    feat: implement humanizeNumber filter

--- client/app/common/filters/humanizeNumber.filter.js
+++ client/app/common/filters/humanizeNumber.filter.js
@@ -3,6 +3,12 @@ function humanizeNumberFilter() {
         if (input < 1000) {
           return String(input);
         }
+        if (input >= 1000 && input < 999500) {
+          return String(Math.round(input / 100) / 10) + 'K';
+        }
+        if (input >= 999500) {
+          return String(Math.round(input / 100000) / 10) + 'M';
+        }
     }
 }
 
--- client/app/common/filters/humanizeNumber.filter.spec.js
+++ client/app/common/filters/humanizeNumber.filter.spec.js
@@ -9,8 +9,23 @@ describe('humanizeNumber filter', function() {
   }));
 
   it('should leave numbers less than 1000 as they are', function() {
-    const i = 1;
-    const humanized = humanizeNumber(1);
-    expect(humanized).to.equal('1');
+    expect(humanizeNumber(1)).to.equal('1');
+    expect(humanizeNumber(10)).to.equal('10');
+    expect(humanizeNumber(123)).to.equal('123');
+    expect(humanizeNumber(999)).to.equal('999');
+  });
+
+  it('should should display numbers between 1000 and 999500 with K suffix (like 1.2K, or 10.3K)', function() {
+    expect(humanizeNumber(1000)).to.equal('1K');
+    expect(humanizeNumber(1010)).to.equal('1K');
+    expect(humanizeNumber(1100)).to.equal('1.1K');
+    expect(humanizeNumber(999450)).to.equal('999.5K');
+  });
+
+  it('should display numbers bigger than 999500 with M suffix', function() {
+    expect(humanizeNumber(999999)).to.equal('1M');
+    expect(humanizeNumber(1999999)).to.equal('2M');
+    expect(humanizeNumber(2399999)).to.equal('2.4M');
+    expect(humanizeNumber(24399999)).to.equal('24.4M');
   });
 });
