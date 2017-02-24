db514905608b1d7f8d167c8b260c927caa4cdd60
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2017-02-24 11:23:22 +0200

    feat: use humanizeNumber filter on feedItem

--- client/app/videoFeed/videoFeed.js
+++ client/app/videoFeed/videoFeed.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+import common from '../common/common';
 import videoFeedItem from './videoFeedItem/videoFeedItem';
 import videoPlayer from './videoPlayer/videoPlayer';
 import videoFeedComponent from './videoFeed.component';
@@ -7,6 +8,7 @@ import videoFeedService from './videoFeed.service';
 
 const videoFeedModule = angular.module('videoFeed', [
   uiRouter,
+  common,
   videoFeedItem,
   videoPlayer
 ])
@@ -18,7 +20,7 @@ const videoFeedModule = angular.module('videoFeed', [
       component: 'videoFeed'
     });
 });
-  
+
 videoFeedComponent.register(videoFeedModule);
 videoFeedService.register(videoFeedModule);
 
